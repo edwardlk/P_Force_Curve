8447d8fd933056f36efc6770bf135defe7e86d85
edwardlk <e.kramkowski@gmail.com>
2014-12-12 23:23:24 -0500

    add singleCurve.py

--- singleCurve.py
+++ singleCurve.py
@@ -1,6 +1,14 @@
 from os import *
 import math
 import numpy as np
-from pylab import *
 import matplotlib.pyplot as plt
-import Tkinter, tkFileDialog
+# import Tkinter, tkFileDialog
+
+# Import Data
+# Col 1 - x-axis data in meters
+# Col 2+ - alternating approach/retract curves, in nanometers
+data = np.genfromtxt("C:\Users\Ed\Documents\GitHub\P_Force_Curve\Spec1.txt", \
+skip_header = 20)
+
+print "rows = ", data.shape[0]
+print "columns = ", data.shape[1]
