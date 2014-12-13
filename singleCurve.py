14d82809c8bfb740cba25bae26d7ad5b294354d4
edwardlk <e.kramkowski@gmail.com>
2014-12-13 13:42:16 -0500

    bring in and plot data

--- singleCurve.py
+++ singleCurve.py
@@ -5,10 +5,19 @@ import matplotlib.pyplot as plt
 # import Tkinter, tkFileDialog
 
 # Import Data
-# Col 1 - x-axis data in meters
-# Col 2+ - alternating approach/retract curves, in nanometers
+# Row 0 - x-axis data in meters, then convert to nm
+# Row 1+ - alternating approach/retract curves, in nanometers
 data = np.genfromtxt("C:\Users\Ed\Documents\GitHub\P_Force_Curve\Spec1.txt", \
-skip_header = 20)
+                     skip_header = 20)
+data_t = np.transpose(data)
+data_t[0] = 10**9 * data_t[0]
 
-print "rows = ", data.shape[0]
-print "columns = ", data.shape[1]
+Fcurves = (data_t.shape[0]-1)/2
+
+print data_t[2].argmax()
+
+plt.figure()
+plt.plot(data_t[0], data_t[2], 'r')
+
+plt.show()
+plt.close()
