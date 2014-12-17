f017719b5e04e1759306e40d9337bb08912f8eda
edwardlk <e.kramkowski@gmail.com>
2014-12-17 17:27:32 -0500

    add curve fitting, adjust plot

--- singleCurve.py
+++ singleCurve.py
@@ -2,7 +2,11 @@ from os import *
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 # import Tkinter, tkFileDialog
+
+def linearF(x, m, b):
+    return m * x + b
 
 # Import Data
 # Row 0 - x-axis data in meters, then convert to nm
@@ -14,27 +18,30 @@ data_t[0] = 10**9 * data_t[0]
 
 numCurves = (data_t.shape[0]-1)/2
 
-maxPos = data_t[2].argmax()
-minPos = data_t[2].argmin()
+maxPos = data_t[4].argmax()
+minPos = data_t[4].argmin()
+
+print "MinPos = ", minPos
+print "MaxPos = ", maxPos
 
 data_t[4] = data_t[4]/0.059
-data_t[4] = data_t[4] - data_t[4][0]
 
 Force = data_t[4] * 0.035
+
+#Finding & fitting the baseline
+popt, pcov = curve_fit(linearF, data_t[0][0:(minPos-1000)], data_t[4][0:(minPos-1000)])
+print "popt = ", popt[1]
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 ax1.set_xlabel('Distance (nm)')
-ax1.set_ylabel('Deflection (nm)', color='b')
-ax1.plot(data_t[0], data_t[4], 'b')
-for tl in ax1.get_yticklabels():
-    tl.set_color('b')
+ax1.set_ylabel('Deflection (nm)')
+ax1.plot(data_t[0], data_t[4]-popt[1], 'r')
+y1, y2 = ax1.get_ylim()
 
 ax2 = ax1.twinx()
-ax2.set_ylabel('Force (nN)', color='r')
-ax2.plot(data_t[0], Force, 'r')
-for tl in ax2.get_yticklabels():
-    tl.set_color('r')
+ax2.set_ylabel('Force (nN)')
+ax2.set_ylim(y1*0.035, y2*0.035)
 
 plt.show()
 plt.close()
