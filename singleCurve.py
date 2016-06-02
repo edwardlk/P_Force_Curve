06760f982761fa46c9b7adfcaac8ba1452d5c4e6
Ed Kramkowski <e.kramkowski@gmail.com>
2016-06-02 17:34:53 -0400

    clean up rescaled vectors and start func for finding surface

--- singleCurve.py
+++ singleCurve.py
@@ -6,6 +6,29 @@ from os import *
 from scipy import stats
 from scipy.optimize import curve_fit
 
+def multiLinReg(x_data,y_data):
+	print "Running MLR"
+	print len(x_data)
+	midlen = len(x_data)/20000
+	init_points = np.zeros((19999,4))
+	for x in range(19999):
+		init_points[x,0] = (x + 1) * midlen
+	for x in range(19999):
+		s1, i1, r1, p1, se1 = stats.linregress(x_data[:init_points[x,0]], y_data[:init_points[x,0]])
+		s2, i2, r2, p2, se2 = stats.linregress(x_data[init_points[x,0]:], y_data[init_points[x,0]:])
+		init_points[x,1] = r1**2
+		init_points[x,2] = r2**2
+		init_points[x,3] = (r1**2 + r2**2)
+		print x, "completed"
+	maxPos = np.argmax(init_points[900:19000,3])
+	if init_points[maxPos-1,3] > init_points[maxPos+1,3]:
+		bound = maxPos-1
+	else:
+		bound = maxPos+1
+	plt.figure()
+	plt.plot(init_points[:,3])
+	plt.show()
+
 #filePath = "C:\Users\ekram\Desktop\Spec4-0006-output.txt"
 #filePath = "C:\Users\ekram\Desktop\Spec4-0110-output.txt"
 filePath = R"C:\Users\ekram\Desktop\testFolder\Spec4-0474-output.txt"
@@ -21,7 +44,7 @@ deflection = dataFile[:,3]*1000000000   # nm
 
 ## Stiffness
 
-k_L = 0.07896 #N/m
+k_L = 0.034 #N/m
 
 ## Find boundaries of setup, approach, retract regions
 ## using z-piezo position
@@ -100,11 +123,15 @@ for x in range(len(timeCh1)):
 
 ## Rescaled vectors to simplify following functions
 approachT = timeCh1[Vbounds[1,2]:Vbounds[2,2]]
-approachZ = distance[Vbounds[1,2]:Vbounds[2,2]]
+approachZ = 4.77*13*distance[Vbounds[1,2]:Vbounds[2,2]]
 approachD = deflection[Vbounds[1,2]:Vbounds[2,2]]
+approachF = k_L * approachD
 retractT = timeCh1[Vbounds[3,2]:Vbounds[4,2]]
-retractZ = distance[Vbounds[3,2]:Vbounds[4,2]]
+retractZ = 4.77*13*distance[Vbounds[3,2]:Vbounds[4,2]]
 retractD = deflection[Vbounds[3,2]:Vbounds[4,2]]
+retractF = k_L * retractD
+
+multiLinReg(retractZ,retractF)
 
 ## Derivative of Approach curve, with rescaled Z-position
 numPoints1 = len(approachZ)
@@ -119,8 +146,8 @@ for x1 in range(points1):
     if high > numPoints1:
         high = numPoints1
     s1, i1, r1, p1, e1 = stats.linregress(
-        4.77*13*approachZ[low:high], approachD[low:high])
-    x1_points[x1] = 4.77*13*np.mean(approachZ[low:high])
+        approachZ[low:high], approachD[low:high])
+    x1_points[x1] = np.mean(approachZ[low:high])
     y1_points[x1] = s1
     int_points1[x1] = round(s1)
 
@@ -167,8 +194,8 @@ for x2 in range(points2):
     if high > numPoints2:
         high = numPoints2
     s2, i2, r2, p2, e2 = stats.linregress(
-        4.77*13*retractZ[low:high], retractD[low:high])
-    x2_points[x2] = 4.77*13*np.mean(retractZ[low:high])
+        retractZ[low:high], retractD[low:high])
+    x2_points[x2] = np.mean(retractZ[low:high])
     y2_points[x2] = s2
     int_points2[x2] = round(s2)
 
@@ -206,9 +233,9 @@ for x in range(len(Rbound_pts)):
 Rbounds = np.zeros((4,3))
 rBndCnt = 0
 for x in range(len(retractZ)-1,-1,-1):
-    if 4.77*13*retractZ[x-1] > Rbound_pts[rBndCnt,1]:
+    if retractZ[x-1] > Rbound_pts[rBndCnt,1]:
         Rbounds[rBndCnt,0] = x
-        Rbounds[rBndCnt,1] = 4.77*13*retractZ[x]
+        Rbounds[rBndCnt,1] = retractZ[x]
         Rbounds[rBndCnt,2] = retractD[x]
         rBndCnt = rBndCnt + 1
     if rBndCnt > 3:
@@ -216,9 +243,9 @@ for x in range(len(retractZ)-1,-1,-1):
 
 # Find baseline & contact region linear regression
 baselineS, baselineI, baselineR, baselineP, baselineE = stats.linregress(
-        4.77*13*retractZ[Rbounds[1,0]:Rbounds[0,0]], retractD[Rbounds[1,0]:Rbounds[0,0]])
+        retractZ[Rbounds[1,0]:Rbounds[0,0]], retractD[Rbounds[1,0]:Rbounds[0,0]])
 contactS, contactI, contactR, contactP, contactE = stats.linregress(
-        4.77*13*retractZ[Rbounds[3,0]:Rbounds[2,0]], retractD[Rbounds[3,0]:Rbounds[2,0]])
+        retractZ[Rbounds[3,0]:Rbounds[2,0]], retractD[Rbounds[3,0]:Rbounds[2,0]])
 
 x_shift = (baselineI - contactI)/(contactS - baselineS)
 y_shift = contactS * x_shift + contactI
@@ -229,17 +256,17 @@ y_shift = contactS * x_shift + contactI
 ## Setup Speed
 setup1,setup2,setup3,setup4,setup5 = stats.linregress(
     timeCh1[0:Vbounds[0,2]], distance[0:Vbounds[0,2]])
-print "setup v =", abs(setup1*13*4.77), "nm/s"
+print "setup v =", abs(setup1), "nm/s"
 
 ## Approach Speed
 appr1,appr2,appr3,appr4,appr5 = stats.linregress(
     approachT, approachZ)
-print "approch v =", abs(appr1*13*4.77), "nm/s"
+print "approch v =", abs(appr1), "nm/s"
 
 ## Retract Speed
 retr1,retr2,retr3,retr4,retr5 = stats.linregress(
     retractT, retractZ)
-print "retract v =", abs(retr1*13*4.77), "nm/s"
+print "retract v =", abs(retr1), "nm/s"
 
 ## Figures
 
@@ -269,8 +296,8 @@ plt.ylabel("Z-Position (V)")
 
 plt.subplot(2,3,6)
 plt.title("Retract")
-plt.plot(4.77*13*retractZ,retractD)
-plt.plot(4.77*13*retractZ - x_shift,retractD - y_shift)
+plt.plot(retractZ,retractD)
+plt.plot(retractZ - x_shift,retractD - y_shift)
 plt.plot(0, 0, 'ro')
 plt.plot(Rbounds[:,1], Rbounds[:,2], 'ro')
 plt.ylabel("Deflection (nm)")
@@ -285,8 +312,8 @@ plt.xlabel("Z-position (V)")
 
 plt.subplot(2,3,4)
 plt.title("Full Retract")
-plt.plot(4.77*13*retractZ,retractD)
-plt.plot(4.77*13*retractZ - x_shift,retractD - y_shift)
+plt.plot(retractZ,retractD)
+plt.plot(retractZ - x_shift,retractD - y_shift)
 plt.plot(0, 0, 'ro')
 plt.plot(Rbounds[:,1], Rbounds[:,2], 'ro')
 plt.ylabel("Deflection (nm)")
