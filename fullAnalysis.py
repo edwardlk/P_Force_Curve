22ad237edacc47d74b41ca0f8db5ad65b757d354
edwardlk <e.kramkowski@gmail.com>
2019-10-02 16:11:27 -0400

    start moving funcs to separate file

--- fullAnalysis.py
+++ fullAnalysis.py
@@ -10,43 +10,7 @@ from scipy import stats
 from scipy.optimize import curve_fit
 from lmfit import Model
 from polymerModels import WLCmodel, FJCmodel
-
-
-def outputFiles(dataFiles, addon):
-    L = []
-    for x in range(len(dataFiles)):
-        temp = dataFiles[x]
-        L.append(temp[:-4] + addon)
-    return L
-
-
-def smooth(x, window_len, window):
-
-    if x.ndim != 1:
-        raise ValueError('smooth only accepts 1-D array')
-    if x.size < window_len:
-        raise ValueError('input vectoir mut be larger than window size')
-    if window_len < 3:
-        return x
-    if window_len % 2 == 0:
-        raise ValueError('Window length must be an odd number.')
-    if window not in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
-        raise ValueError("Window must be: 'flat', 'hanning', 'hamming', "
-                         "'bartlett', or 'blackman'")
-
-    end = window_len - int(window_len)/2
-
-    s = np.r_[x[window_len-1:0:-1], x, x[-1:-window_len:-1]]
-
-    if window == 'flat':  # moving average
-        w = np.ones(window_len, 'd')
-    else:
-        w = eval('np.'+window+'(window_len)')
-
-    y = np.convolve(w/w.sum(), s, mode='valid')
-
-    return y[int(window_len-end):int(-window_len+end)]
-
+from PFCfuncs import outputFiles, smooth
 
 # Designate input and output directories.
 root = Tk()
